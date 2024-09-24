f1e6715fad0db51f47aeb451fce211edd52f3895
Teraby1 <152976997+Teraby1@users.noreply.github.com>
2024-09-24 15:42:53 +0200

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1HFYpHZ0mb5CPzJfNkPEG-q9j__L4cl5UEDJuYkPj1T4";
 
 
 /* ------------------------------------------------------------------- */
